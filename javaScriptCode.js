02c0b49b82977969d24d68fef97d1a70c795e2cc
Gabriel Oliver <gabriel-a-oliver@users.noreply.github.com>
2020-05-28 19:50:27 -0700

    stufffff

--- javaScriptCode.js
+++ javaScriptCode.js
@@ -1,9 +1,25 @@
 /*var testNumber = "Hello!!";
 document.getElementById("test").innerHTML = "Hello!!!!!";*/
-
+let pets = ["Bella", "Buck", "Lily", "Soba"];
 function main() {
 
-    var testNumber = "Hello!!";
+    var testNumber = "Variable Requirement Checked!!";
     document.getElementById("test").innerHTML = testNumber;
+};
+
+function carouselTimer() {
+    document.getElementById('hiddencarousel').style.visibility = 'visible';
+    setTimeout(function(){document.getElementById('hiddencarousel').style.visibility = 'hidden'}, 10000);
+};
+
+/*function showCarousel() {
+    document.getElementById('hiddencarousel').style.visibility = 'visible';
+};
+
+function hideCarousel() {
+    document.getElementById('hiddencarousel').style.visibility = 'hidden';
+};*/
+
+function tempShadow() {
 
 };
