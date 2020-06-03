c8066cad04c82452efb0715874df96bb4255aa98
Gabriel Oliver <gabriel-a-oliver@users.noreply.github.com>
2020-06-03 12:29:32 -0700

    added comments, reduced redundancy

--- javaScriptCode.js
+++ javaScriptCode.js
@@ -3,16 +3,15 @@ document.getElementById("test").innerHTML = "Hello!!!!!";*/
 
 
 
-
+// Pre: Function loads when the webpage is opened.
+//
+// Post: Displays the website header and names of our pets.
 function main() {
-    if (true) {
-        var testNumber = "SHALOM!";
-        document.getElementById("test").innerHTML = testNumber;
-    }
-    
+    // Webpage Title
+    var testNumber = "Our Petz!";
+    document.getElementById("test").innerHTML = testNumber;
 
-    
-
+    // Pets names found on the home page
     let pets = ["Bella", "Buck", "Lily", "Soba"];
     let petsID = ["petName0", "petName1", "petName2", "petName3"];
     for (var i = 0; i < 4; i++) {
@@ -22,31 +21,18 @@ function main() {
             document.getElementById(petsID[i]).style.color = "whitesmoke";
         }
     }
-
-
 };
 
+// Paramater: bool mouseHoverOver, true if mouse is hovering over the button that
+//            runs this function.
+//
+// Post: Makes a carousel of images visible while the mouse is hovering over button.
+//       Becomes hidden when not hovering over button.
 function carouselTimer(mouseHoverOver) {
-    
     document.getElementById('hiddencarousel').style.visibility = 'visible';
-    console.log("in loop");
     if (mouseHoverOver) {
         document.getElementById('hiddencarousel').style.visibility = 'visible';
     } else {
         document.getElementById('hiddencarousel').style.visibility = 'hidden';
     }
-    /*
-    while (mouseHoverOver) {
-        
-        document.getElementById('hiddencarousel').style.visibility = 'visible';
-        
-    }*/
-
-    
-/*
-    
-    document.getElementById('hiddencarousel').style.visibility = 'visible';
-    
-    setTimeout(function(){document.getElementById('hiddencarousel').style.visibility = 'hidden'}, 10000);
-    */
 };
