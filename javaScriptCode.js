9d198235835fe92ada845d4631f1f6fcd38ce4ec
Gabriel Oliver <gabriel-a-oliver@users.noreply.github.com>
2020-06-02 19:57:41 -0700

    completed requirements

--- javaScriptCode.js
+++ javaScriptCode.js
@@ -1,18 +1,52 @@
 /*var testNumber = "Hello!!";
 document.getElementById("test").innerHTML = "Hello!!!!!";*/
-let pets = ["Bella", "Buck", "Lily", "Soba"];
+
+
+
+
 function main() {
     if (true) {
         var testNumber = "SHALOM!";
         document.getElementById("test").innerHTML = testNumber;
     }
+    
+
+    
+
+    let pets = ["Bella", "Buck", "Lily", "Soba"];
+    let petsID = ["petName0", "petName1", "petName2", "petName3"];
+    for (var i = 0; i < 4; i++) {
+        console.log(petsID[i]);
+        document.getElementById(petsID[i]).innerHTML = pets[i];
+        if(i == 3) {
+            document.getElementById(petsID[i]).style.color = "whitesmoke";
+        }
+    }
+
+
 };
 
-function carouselTimer() {
+function carouselTimer(mouseHoverOver) {
+    
     document.getElementById('hiddencarousel').style.visibility = 'visible';
+    console.log("in loop");
+    if (mouseHoverOver) {
+        document.getElementById('hiddencarousel').style.visibility = 'visible';
+    } else {
+        document.getElementById('hiddencarousel').style.visibility = 'hidden';
+    }
+    /*
+    while (mouseHoverOver) {
+        
+        document.getElementById('hiddencarousel').style.visibility = 'visible';
+        
+    }*/
+
+    
+/*
+    
+    document.getElementById('hiddencarousel').style.visibility = 'visible';
+    
     setTimeout(function(){document.getElementById('hiddencarousel').style.visibility = 'hidden'}, 10000);
-};
-
-function tempShadow() {
-
+    */
 };
